69973fe3855606f08b2250ef4667c8d9f631401b
Brian Omondi <brianhilsden@gmail.com>
2024-05-01 12:51:53 +0300

    changed database

--- jobs/database.py
+++ jobs/database.py
@@ -42,8 +42,8 @@ class Applicants(db.Model):
     return bcrypt.check_password_hash(self.passwordApp, attempted_password)
 
 
-engine = create_engine("mysql+pymysql://root:-BgbEEE646GGeEF-Fg316DAG2ff55Gce@roundhouse.proxy.rlwy.net:51741/railway?charset=utf8mb4")
-""" engine = create_engine("mysql+pymysql://avnadmin:AVNS_cVN41Ne4egx4yWk35tZ@mysql-7a9d636-royalty-inc20.a.aivencloud.com:16051/defaultdb?charset=utf8mb4") """
+""" engine = create_engine("mysql+pymysql://root:-BgbEEE646GGeEF-Fg316DAG2ff55Gce@roundhouse.proxy.rlwy.net:51741/railway?charset=utf8mb4") """
+engine = create_engine("mysql+pymysql://avnadmin:AVNS_cVN41Ne4egx4yWk35tZ@mysql-7a9d636-royalty-inc20.a.aivencloud.com:16051/defaultdb?charset=utf8mb4")
 """ engine = create_engine("mysql+pymysql://root:Royalty20@127.0.0.1:3306/sys?charset=utf8mb4") """
 
 def load_jobs_from_db():
